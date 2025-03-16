8f0f16f168238419ba22a35066aff14df5c75312
Rahul <imrahulg01@gmail.com>
2025-03-16 22:34:37 +0530

    feat: enhance OrderSerializer to filter validated data for Order creation

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,7 @@ from .models import PurchaseOrder, Order
 import base64
 
 class PurchaseOrderSerializer(serializers.ModelSerializer):
+    
     available_quantity = serializers.IntegerField(required=False)
     class Meta:
         model = PurchaseOrder
@@ -13,7 +14,14 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
             validated_data['available_quantity'] = validated_data['quantity']
         return super().create(validated_data)
 
+
 class OrderSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Order
         fields = '__all__'
+
+    def create(self, validated_data):
+        model_fields = {field.name for field in Order._meta.fields}
+        filtered_data = {k: v for k, v in validated_data.items() if k in model_fields}        
+        return Order.objects.create(**filtered_data)
