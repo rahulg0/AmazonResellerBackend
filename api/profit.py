84e24a4df48d3d3a09b37e7b65941f3c9fb211ac
Rahul <imrahulg01@gmail.com>
2025-03-16 22:34:08 +0530

    feat: implement Amazon order retrieval and profit calculation functionality

---
+++ api/profit.py
@@ -0,0 +1,134 @@
+import requests
+import os
+import time
+import json
+from decimal import Decimal
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
+from api.models import *
+from api.serializers import *
+from celery import shared_task
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+load_dotenv()
+
+BASE_URL=os.getenv("BASE_URL")
+REFRESH_URL=os.getenv("REFRESH_URL")
+CLIENT_ID=os.getenv("CLIENT_ID")
+CLIENT_SECRET=os.getenv("CLIENT_SECRET")
+REFRESH_TOKEN=os.getenv("REFRESH_TOKEN")
+MARKETPLACE_ID=os.getenv("MARKETPLACE_ID")
+
+def get_amazon_oauth_token(refresh_token,client_id,client_secret):
+    print("taking auth")
+    url=REFRESH_URL
+    headers={"Content-Type": "application/x-www-form-urlencoded"}
+    data={
+        "grant_type":"refresh_token",
+        "refresh_token":refresh_token,
+        "client_id":client_id,
+        "client_secret":client_secret
+    }
+    response = requests.post(url, headers=headers, data=data)
+    return response.json().get("access_token")
+
+def get_data(access_token, a_id, retries=3, retry_delay=2):
+    url = f"{BASE_URL}/finances/v0/orders/{a_id}/financialEvents"
+    headers = {
+        "x-amz-access-token": access_token
+    }
+    for attempt in range(retries):
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json().get("payload", {}).get("FinancialEvents", [])
+            return data if data else None
+        if response.status_code == 429:
+            if attempt < retries - 1:
+                time.sleep(retry_delay)
+            else:
+                raise Exception("Rate limit exceeded, retries exhausted.")
+        else:
+            response.raise_for_status()
+    return None
+
+def update_profit():
+    access_token = get_amazon_oauth_token(REFRESH_TOKEN, CLIENT_ID, CLIENT_SECRET)
+    all_orders = Order.objects.filter(have_profit=False).values_list("AmazonOrderId", "ASIN", "QuantityShipped").iterator()
+    for orders in all_orders:
+        amazon_order_id, asin, quantity = orders
+        pos = PurchaseOrder.objects.filter(asin=asin).order_by('created_at')
+        COG = 0
+        remaining_quantity = quantity
+        used_pos = []
+        for po in pos:
+            if remaining_quantity <= 0:
+                break
+            if po.available_quantity == 0:
+                continue
+            used_quantity = min(remaining_quantity, po.available_quantity)
+            COG += used_quantity * po.amount_per_unit
+            remaining_quantity -= used_quantity
+            used_pos.append((po, used_quantity))
+        if remaining_quantity > 0:
+            continue
+        data = get_data(access_token, amazon_order_id)
+        if 'ShipmentEventList' in data:
+            shipment_events = data['ShipmentEventList']
+            if shipment_events:  # Ensure it's not empty
+                shipment = shipment_events[0]  # Get first shipment event
+                shipment_items = shipment.get('ShipmentItemList', [])  # Get ShipmentItemList safely
+            else:
+                print("No Shipment")
+                continue
+            print("Have shipment events")
+        else:
+            print("No shipment")
+            continue
+        print("check1")
+        shipment = shipment_items[0]
+        # print(json.dumps(shipment, indent=3))
+        principal,shipping_charge,fba_fee,commission,promotion_discount = 0,0,0,0,0
+        final_profit=0
+        print
+        try:
+            itemchargeList = shipment['ItemChargeList'] if 'ItemChargeList' in shipment else []
+            ItemFeeList = shipment['ItemFeeList'] if 'ItemFeeList' in shipment else []
+            PromotionList = shipment['PromotionList'] if 'PromotionList' in shipment else []
+        except KeyError as e:
+            print(f"KeyError: {e} not found in shipment")
+        if itemchargeList:
+            principal = next((charge["ChargeAmount"]["CurrencyAmount"] for charge in itemchargeList if charge["ChargeType"] == "Principal"), 0)
+            shipping_charge = next((charge["ChargeAmount"]["CurrencyAmount"] for charge in itemchargeList if charge["ChargeType"] == "ShippingCharge"), 0)
+        if ItemFeeList:
+            fba_fee = next((fee["FeeAmount"]["CurrencyAmount"] for fee in ItemFeeList if fee["FeeType"] == "FBAPerUnitFulfillmentFee"), 0)
+            commission = next((fee["FeeAmount"]["CurrencyAmount"] for fee in ItemFeeList if fee["FeeType"] == "Commission"), 0)
+        if PromotionList:
+            promotion_discount = next((promo["PromotionAmount"]["CurrencyAmount"] for promo in PromotionList if promo["PromotionAmount"]["CurrencyAmount"] != 0), 0)
+        print(amazon_order_id)
+        print("principal,fba_fee,commission,shipping_charge,promotion_discount, COG",principal,fba_fee,commission,shipping_charge,promotion_discount,COG)
+        final_profit = principal - float(COG) - abs(fba_fee) - abs(commission) + abs(shipping_charge) - abs(promotion_discount)
+        print("final profit ==", final_profit)
+        for po, used_quantity in used_pos:
+            po.profit += Decimal(final_profit) * Decimal(used_quantity) / Decimal(quantity)
+            po.available_quantity -= used_quantity
+            total_cog = po.amount_per_unit * used_quantity  # Total cost of goods in this PO
+            if total_cog > 0:
+                po.profit_percentage = (po.profit / total_cog) * 100
+            po.save()
+        ord = Order.objects.get(AmazonOrderId=amazon_order_id)
+        ord.profit = final_profit
+        ord.profit_percentage = (Decimal(final_profit) / COG) * 100 if COG > 0 else 0
+        ord.have_profit = True
+        ord.save()
+
+
+#Driver Code
+@shared_task
+def main():
+    update_profit()
---
+++ api/task.py
@@ -0,0 +1,181 @@
+import requests
+from celery import shared_task
+import os
+import time
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
+from api.models import *
+from api.serializers import *
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+load_dotenv()
+
+BASE_URL=os.getenv("BASE_URL")
+REFRESH_URL=os.getenv("REFRESH_URL")
+CLIENT_ID=os.getenv("CLIENT_ID")
+CLIENT_SECRET=os.getenv("CLIENT_SECRET")
+REFRESH_TOKEN=os.getenv("REFRESH_TOKEN")
+MARKETPLACE_ID=os.getenv("MARKETPLACE_ID")
+
+
+def get_amazon_oauth_token(refresh_token,client_id,client_secret):
+    print("taking auth")
+    url=REFRESH_URL
+    headers={"Content-Type": "application/x-www-form-urlencoded"}
+    data={
+        "grant_type":"refresh_token",
+        "refresh_token":refresh_token,
+        "client_id":client_id,
+        "client_secret":client_secret
+    }
+    response = requests.post(url, headers=headers, data=data)
+    return response.json().get("access_token")
+
+
+def get_amazon_orders(access_token):
+    try:
+        print("inside getting amazon orders")
+        url = BASE_URL + "/orders/v0/orders"
+        created_after = (datetime.now(timezone.utc) - timedelta(hours=2)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        params = {
+            "CreatedAfter": created_after,
+            "OrderStatus": "Shipped",
+            "MarketplaceIds": MARKETPLACE_ID
+        }
+        headers = {
+            "accept": "application/json",
+            "x-amz-access-token": access_token
+        }
+        all_orders = []
+        request_count = 0
+        while True:
+            if request_count >= 20:  
+                print("Burst limit reached, waiting 60 seconds...")
+                time.sleep(60)  
+                request_count = 0  
+            response = requests.get(url, headers=headers, params=params)
+            data = response.json().get("payload", {})
+            print(data)
+            if "Orders" in data:
+                all_orders.extend(data["Orders"])
+            if "NextToken" in data:
+                print("Fetching next page...")
+                params = {"NextToken": data["NextToken"]}
+                url = BASE_URL + "/orders/v0/orders"
+            else:
+                break
+            request_count += 1
+        return all_orders
+    except Exception as e:
+        print("Exception in AO:", str(e))
+
+
+def get_details(access_token):
+    try:
+        all_orders = get_amazon_orders(access_token)
+        print("inside getting details")
+        headers = {"x-amz-access-token": access_token}
+        print("size of all orders: ",len(all_orders))
+        request_count = 0
+        for order in all_orders:
+            a_id = order['AmazonOrderId']
+            url = BASE_URL + f'/orders/v0/orders/{a_id}/orderItems'
+            if request_count >= 30:
+                print("Burst limit reached, waiting 2 seconds per request...")
+                time.sleep(2)
+            resp = requests.get(url, headers=headers)
+            if resp.status_code == 200:
+                data = resp.json().get('payload', {})
+                order_item = data.get('OrderItems', {})[0]
+                if isinstance(order_item, dict):
+                    order.update(order_item)
+                else:
+                    logger.error(f"Unexpected data format for OrderItems in {a_id}")
+            else:
+                logger.error(f"Failed to fetch details for {a_id}, Status Code: {resp.status_code}")
+            request_count += 1
+        return all_orders
+    except Exception as e:
+        print("Exception in GD: ",str(e))
+
+def is_asin_present(asin):
+    return PurchaseOrder.objects.filter(asin=asin).exists()
+
+def check_quantity(asin, quantity):
+    try:
+        item = OrderItem.objects.get(ASIN=asin)        
+        if item and item.QuantityLeft < quantity:
+            return False        
+        item.QuantityLeft -= quantity
+        item.save()
+        return True
+    except OrderItem.DoesNotExist:
+        return "ItemNotFound"
+
+def add_order_to_db(access_token):
+    try:
+        orders = get_details(access_token)
+        print("adding order to db")
+        error_orders = []
+        serialized_data = []
+
+        orders_data = orders if isinstance(orders, list) else [orders]
+
+        for data in orders_data:
+            print(data)
+            asin = data.get("ASIN")
+
+            if is_asin_present(asin):
+                amazon_order_id = data.get("AmazonOrderId")
+
+                pack_of = int(PurchaseOrder.objects.get(asin=asin).pack_of)
+                quantity = data.get("NumberOfItemsShipped", 0) * pack_of
+                selling_price = data.get("ItemPrice", {}).get("Amount")
+
+                if not Order.objects.filter(AmazonOrderId=amazon_order_id).exists():
+                    quantity_status = check_quantity(asin, quantity)
+
+                    if quantity_status is True and selling_price is not None:
+                        logger.info(f"Valid order received for ASIN {asin}")
+                        serialized_data.append(data)
+                    else:
+                        error_orders.append(ErrorOrders(
+                            id_type="AmazonOrderId" if quantity_status != "ItemNotFound" else "ASIN",
+                            id_value=amazon_order_id if quantity_status != "ItemNotFound" else asin,
+                            data=data
+                        ))
+                else:
+                    error_orders.append(ErrorOrders(
+                        id_type="AmazonOrderId",
+                        id_value=amazon_order_id,
+                        data=data
+                    ))
+            else:
+                error_orders.append(ErrorOrders(
+                    id_type="ASIN",
+                    id_value=asin,
+                    data=data
+                ))
+
+        serializer = OrderSerializer(data=serialized_data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            logger.info("Orders saved successfully")
+
+        if error_orders:
+            ErrorOrders.objects.bulk_create(error_orders)
+            logger.info(f"Saved {len(error_orders)} error orders")
+    except Exception as e:
+        print("Exception in AOD: ",str(e))
+
+@shared_task
+def main():
+    print("pilot.................")
+    access_token=get_amazon_oauth_token(REFRESH_TOKEN, CLIENT_ID, CLIENT_SECRET)
+    add_order_to_db(access_token)
